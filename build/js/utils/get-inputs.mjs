21e5b7cad3e22d9950cf3daec1d7cd62c4a02c17
3eze3 <ezekics33@gmail.com>
2025-04-11 15:10:33 -0500

    Add the getinputs

---
+++ build/js/utils/get-inputs.mjs
@@ -0,0 +1,8 @@
+export function getInputs() {
+    const $inputs = document.querySelectorAll('.form__input');
+    return $inputs;
+}
+export function getInputsRadio() {
+    const $inputs = document.querySelectorAll('.form__radio');
+    return $inputs;
+}
